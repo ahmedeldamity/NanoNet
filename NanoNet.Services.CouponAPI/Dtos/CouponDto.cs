e4f95dd6a968b007f3d14f564a7065ee8367c882
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-07-27 01:47:44 +0300

    Added Coupon API with models and DTOs

---
+++ NanoNet.Services.CouponAPI/Dtos/CouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NanoNet.Services.CouponAPI.Dtos
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
