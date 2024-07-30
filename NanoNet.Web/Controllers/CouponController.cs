71072ed279c1eada036e9f9cf12ce4477bbed779
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-07-31 00:08:59 +0300

    Add Coupon controller with CouponIndex action and its view

---
+++ NanoNet.Web/Controllers/CouponController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NanoNet.Web.Interfaces.IService;
+using NanoNet.Web.ViewModels;
+using Newtonsoft.Json;
+
+namespace NanoNet.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponViewModel>? list = new();
+
+            ResponseViewModel? response = await _couponService.GetAllCouponsAsync();
+
+            if (response is not null && response.IsSuccess)
+            {
+                var jsonData = Convert.ToString(response.Result);
+                list = JsonConvert.DeserializeObject<List<CouponViewModel>>(jsonData);
+            }
+
+            return View(list);
+        }
+    }
+}
