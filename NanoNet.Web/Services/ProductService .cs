15fc710b63ff33f0c5cc68d95e3849fe4168a5fa
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-16 19:45:03 +0300

    Get All Prodcus Service In Web Project

---
+++ NanoNet.Web/Services/ProductService .cs
@@ -0,0 +1,19 @@
+﻿using NanoNet.Web.Interfaces.IService;
+using NanoNet.Web.Utility;
+using NanoNet.Web.ViewModels;
+
+namespace NanoNet.Web.Services
+{
+    public class ProductService(IBaseService _baseService) : IProductService
+    {
+        public async Task<ResponseViewModel?> GetAllProductsAsync()
+        {
+            return await _baseService.SendAsync(new RequestViewModel()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.CouponAPIBase + "/api/product"
+            });
+        }
+  
+    }
+}
