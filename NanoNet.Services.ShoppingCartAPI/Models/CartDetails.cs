e32016b7adc11849e91f30157a1df3aacbc0eeca
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-18 03:39:17 +0300

    Add shopping cart models

---
+++ NanoNet.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,21 @@
+﻿using NanoNet.Services.ShoppingCartAPI.Dtos;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NanoNet.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        public int Id { get; set; }
+
+        public int CartHeaderId { get; set; }
+
+        public CartHeader CartHeader { get; set; }
+        
+        public int ProductId { get; set; }
+
+        [NotMapped]
+        public ProductDto Product { get; set; }
+
+        public int Count { get; set; }
+    }
+}
