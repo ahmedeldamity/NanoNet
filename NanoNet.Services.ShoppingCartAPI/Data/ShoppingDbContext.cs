d8ac4c47c057771203e6c95a539b5fe36011ced1
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-18 04:17:31 +0300

    Add Shopping cart tables

--- NanoNet.Services.ShoppingCartAPI/Data/ShoppingDbContext.cs
+++ NanoNet.Services.ShoppingCartAPI/Data/ShoppingDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NanoNet.Services.ShoppingCartAPI.Models;
 
 namespace NanoNet.Services.ShoppingCartAPI.Data
 {
     public class ShoppingDbContext: DbContext
     {
         public ShoppingDbContext(DbContextOptions<ShoppingDbContext> options): base(options) { }
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
     }
 }
