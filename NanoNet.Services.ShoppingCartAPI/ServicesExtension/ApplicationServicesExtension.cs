f162b3298b146c7757719bc984e72963fee54801
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-20 20:17:26 +0300

    Send token in the request header using delegating handlers

--- NanoNet.Services.ShoppingCartAPI/ServicesExtension/ApplicationServicesExtension.cs
+++ NanoNet.Services.ShoppingCartAPI/ServicesExtension/ApplicationServicesExtension.cs
@@ -3,6 +3,7 @@ using NanoNet.Services.ShoppingCartAPI.Helpers;
 using NanoNet.Services.ShoppingCartAPI.Interfaces.IService;
 using NanoNet.Services.ShoppingCartAPI.Services;
 using NanoNet.Services.ShoppingCartAPI.SettingData;
+using NanoNet.Services.ShoppingCartAPI.Utility;
 
 namespace NanoNet.Services.ShoppingCartAPI.ServicesExtension
 {
@@ -13,22 +14,25 @@ namespace NanoNet.Services.ShoppingCartAPI.ServicesExtension
             var serviceProvider = services.BuildServiceProvider();
             var apiData = serviceProvider.GetRequiredService<IOptions<APIsUrl>>().Value;
 
+            services.AddHttpContextAccessor();
 
             services.AddHttpClient("Product", client =>
             {
                 client.BaseAddress = new Uri(apiData.ProductAPI);
-            });
+            }).AddHttpMessageHandler<AuthenticationHttpClientHandler>();
 
             services.AddHttpClient("Coupon", client =>
             {
                 client.BaseAddress = new Uri(apiData.CouponAPI);
-            });
+            }).AddHttpMessageHandler<AuthenticationHttpClientHandler>();
 
             services.AddAutoMapper(typeof(MappingConfig));
 
             services.AddScoped<IProductService, ProductService>();
 
             services.AddScoped<ICouponService, CouponService>();
+
+            services.AddScoped<AuthenticationHttpClientHandler>();
 
             return services;
         }
---
+++ NanoNet.Services.ShoppingCartAPI/Utility/AuthenticationHttpClientHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+
+namespace NanoNet.Services.ShoppingCartAPI.Utility;
+public class AuthenticationHttpClientHandler(IHttpContextAccessor _accessor): DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var token = await _accessor.HttpContext!.GetTokenAsync("access_token");
+        if (!string.IsNullOrEmpty(token))
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
