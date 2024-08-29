d55e33ed145389d9a96a67bac8ee73afe7fce22c
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-08-30 00:04:14 +0300

    Update database dinamically

--- NanoNet.Services.OrderAPI/Program.cs
+++ NanoNet.Services.OrderAPI/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using NanoNet.Services.OrderApi.ServicesExtension;
+using NanoNet.Services.OrderAPI.Data;
 using NanoNet.Services.OrderAPI.ServicesExtension;
 using NanoNet.Services.ShoppingCartAPI.ServicesExtension;
 
@@ -21,6 +23,35 @@ builder.Services.AddApplicationServices();
 #endregion
 
 var app = builder.Build();
+
+#region Update Database With Using Way And Seeding Data
+
+// We Said To Update Database You Should Do Two Things (1. Create Instance From DbContext 2. Migrate It)
+
+// To Ask Clr To Create Instance Explicitly From Any Class
+//    1 ->  Create Scope (Life Time Per Request)
+using var scope = app.Services.CreateScope();
+//    2 ->  Bring Service Provider Of This Scope
+var services = scope.ServiceProvider;
+
+// --> Bring Object Of OrderDbContext For Update His Migration And Data Seeding
+var _orderContext = services.GetRequiredService<OrderDbContext>();
+
+// --> Bring Object Of ILoggerFactory For Good Show Error In Console    
+var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+try
+{
+    // Migrate OrderDbContext
+    await _orderContext.Database.MigrateAsync();
+}
+catch (Exception ex)
+{
+    var logger = loggerFactory.CreateLogger<Program>();
+    logger.LogError(ex, "an error has been occured during apply the migration!");
+}
+
+#endregion
 
 #region Configure the Kestrel pipeline
 
