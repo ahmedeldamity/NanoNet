3bc8eb1f012e67e911e152b957a384eb04fd2f31
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-09-13 03:05:18 +0300

    Configure Stripe And Create Stripe Session in Order API

--- NanoNet.Services.OrderAPI/Controllers/OrderController.cs
+++ NanoNet.Services.OrderAPI/Controllers/OrderController.cs
@@ -5,10 +5,12 @@ using NanoNet.Services.OrderAPI.Data;
 using NanoNet.Services.OrderAPI.Dtos;
 using NanoNet.Services.OrderAPI.Models;
 using NanoNet.Services.OrderAPI.Utility;
+using Stripe;
+using Stripe.Checkout;
 
 namespace NanoNet.Services.OrderAPI.Controllers
 {
-    public class OrderController(IMapper _mapper, OrderDbContext _orderDbContext) : BaseController
+    public class OrderController(IMapper _mapper, OrderDbContext _orderDbContext, IConfiguration _configuration) : BaseController
     {
         [Authorize]
         [HttpPost("create-order")]
@@ -37,6 +39,58 @@ namespace NanoNet.Services.OrderAPI.Controllers
             {
                 response.IsSuccess = false;
                 response.Message = e.Message;
+            }
+            return response;
+        }
+
+        [Authorize]
+        [HttpPost("CreateStripeSession")]
+        public async Task<ResponseDto> CreateStripeSession(StripeRequestDto stripeRequestDto)
+        {
+            ResponseDto response = new();
+            try
+            {
+                StripeConfiguration.ApiKey = _configuration["StripeSettings:Secretkey"];
+
+                var options = new SessionCreateOptions
+                {
+                    SuccessUrl = stripeRequestDto.StripeApprovedUrl,
+                    CancelUrl = stripeRequestDto.StripeCancelledUrl,
+                    LineItems = [],
+                    Mode = "payment",
+                };
+
+
+                foreach (var item in stripeRequestDto.OrderHeader.OrderItems)
+                {
+                    var sessionLineItem = new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long) (item.Price*100),
+                            Currency = "usd",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = item.Product!.Name
+                            }
+                        },
+                        Quantity = item.Count
+                    };
+                    options.LineItems.Add(sessionLineItem);
+                }
+
+                var service = new SessionService();
+                Session session = service.Create(options);
+                stripeRequestDto.StripeSessionUrl = session.Url;
+                OrderHeader orderHeader = _orderDbContext.OrderHeaders.First(u => u.Id == stripeRequestDto.OrderHeader.Id);
+                orderHeader.StripeSessionId = session.Id;
+                await _orderDbContext.SaveChangesAsync();
+                response.Result = stripeRequestDto;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
             }
             return response;
         }
--- NanoNet.Services.OrderAPI/Dtos/StripeRequestDto.cs
+++ NanoNet.Services.OrderAPI/Dtos/StripeRequestDto.cs
@@ -4,6 +4,6 @@ public class StripeRequestDto
     public string StripeSessionUrl { get; set; } = null!;
     public string StripeSessionId { get; set; } = null!;
     public string StripeApprovedUrl { get; set; } = null!;
-    public string StripeCancelUrl { get; set; } = null!;
+    public string StripeCancelledUrl { get; set; } = null!;
     public OrderHeaderDto OrderHeader { get; set; } = null!;
 }
